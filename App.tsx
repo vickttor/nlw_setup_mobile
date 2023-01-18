a27e9238e66c201e76bf5e4b5ec14cfe79ce13fc
Victor Silva <victor.silvaPC@hotmail.com>
2023-01-18 20:50:19 -0300

    :zap: feat: create home screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
-
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
-import { Loading } from './src/components/Loading';
-import { 
-  useFonts, 
-  Inter_400Regular, 
-  Inter_600SemiBold, 
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold
+  Inter_800ExtraBold, useFonts
 } from "@expo-google-fonts/inter";
-
-
+import { StatusBar } from 'react-native';
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
 
@@ -25,24 +22,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Habits</Text>
+    <>
+      <Home/>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#7C3AED',
-    fontFamily: 'Inter_800ExtraBold',
-    fontSize: 60
-  }
-});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,57 @@
+import { View, Text, ScrollView } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import {generateRangeDatesFromYearStart} from "../utils/generate-range-between-dates"
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesfromYearStart = generateRangeDatesFromYearStart();
+
+const minimumSummaryDateSizes = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDateSizes - datesfromYearStart.length;
+
+export function Home(){
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      {/* HEADER */}
+      <Header/>
+    
+      {/* DAY OF THE WEEK */}
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, index)=> {
+          return (
+            <Text 
+              key={`${weekDay}-${index}`} 
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{width: DAY_SIZE}}
+            >
+              {weekDay}
+            </Text>
+          )
+        })}
+      </View>
+
+      {/* DAYS */}
+      <ScrollView 
+        showsVerticalScrollIndicator={false} 
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        <View className="flex-row flex-wrap">
+          { 
+            datesfromYearStart.map((date)=> {
+            return <HabitDay
+                key={date.toISOString()}
+              />
+            })
+          }
+
+          { 
+            amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill})
+              .map((_, index)=> {
+                return <View className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40" style={{width: DAY_SIZE, height: DAY_SIZE}}/>
+              })
+          }
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
