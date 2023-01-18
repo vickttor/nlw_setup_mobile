78ce9d8feac2757e8a427249fee7b126971d1279
Victor Silva <victor.silva@ax4b.com>
2023-01-18 15:42:03 -0300

    :tada: initial commit

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+
+export function Loading(){
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color="#7C3AED" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#09090A',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
