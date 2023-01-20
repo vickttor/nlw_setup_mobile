d0ac04e1a3086ed3895bef646cb8809dffcaea9d
Victor Silva <victor.silvaPC@hotmail.com>
2023-01-20 11:18:10 -0300

    :zap: feat: created Habit screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -10,6 +11,9 @@ const minimumSummaryDateSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDateSizes - datesfromYearStart.length;
 
 export function Home(){
+
+  const {navigate} = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       {/* HEADER */}
@@ -41,6 +45,7 @@ export function Home(){
             return (
               <HabitDay
                 key={date.toISOString()}
+                onPress={()=> navigate("habit", {date: date.toISOString()})}
               />
             )
             })
